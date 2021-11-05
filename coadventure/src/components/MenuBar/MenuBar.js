061260722ad9eab1fea0f35183bdad6d5257aede
elib <elib@itu.dk>
2021-11-05 14:22:50 +0100

    menubar

--- coadventure/src/components/MenuBar/MenuBar.js
+++ coadventure/src/components/MenuBar/MenuBar.js
@@ -1,5 +1,50 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, NavLink, Switch, BrowserRouter } from "react-router-dom";
+import Home from './MenuBarItems/Home/Home';
+import Excursions from './MenuBarItems/Excursions/Excursions';
+import BookSeats from './MenuBarItems/BookSeats/BookSeats';
+import Feedback from './MenuBarItems/Feedback/Feedback';
+import './MenuBar.css';
 
 function MenuBar(){
-    
+    return(
+        <BrowserRouter>
+            <div className="container">
+                <div className="link">
+                    <NavLink to="/Home" activeStyle>
+                        Home
+                    </NavLink>
+                </div>
+
+                <div className="link">
+                    <NavLink to="/Excursions" activeStyle>
+                        Excursions
+                    </NavLink>
+                </div>
+
+                <div className="link">
+                    <NavLink to="/BookSeats" activeStyle>
+                        Book Seats
+                    </NavLink>
+                </div>
+
+                <div className="link">
+                    <NavLink to="/Feedback" activeStyle>
+                        Feedback
+                    </NavLink>
+                </div>
+            </div>
+
+            <Switch>
+                <Route path="/home" render={Home}/>
+                <Route path="/excursions" render={Excursions}/>
+                <Route path="/bookSeats" render={BookSeats}/>
+                <Route path="/feedback" render={Feedback}/>
+            </Switch>
+        </BrowserRouter>
+
+    )
 }
+
+export default MenuBar;
+
---
+++ coadventure/src/components/MenuBar/MenuBarItems/BookSeats/BookSeats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function BookSeats(){
+    return(
+    <h1>Hello Book Seats</h1>
+    )
+}
+
+export default BookSeats;
+
