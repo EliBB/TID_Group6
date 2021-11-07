a54ca8377c1b5b62ec48de05fc529ba2e2523974
elib <elib@itu.dk>
2021-11-07 13:58:26 +0100

    Member sign up

--- coadventure/src/components/MenuBar/MenuBarItems/Home/Member/Member.js
+++ coadventure/src/components/MenuBar/MenuBarItems/Home/Member/Member.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MemberRegistration from './MemberRegistration';
+
 
 function Member(){
     return(
-        <p>Member</p>
+        <div>
+            <h1>This is the member page</h1>
+            {/* <ScrollableTable/> */}
+            
+            <div>
+                <Link to="/memberRegistration" activeStyle>
+                        Sign Up
+                </Link> 
+            </div>
+
+        </div>            
+
+     
+    
     )
 }
 
