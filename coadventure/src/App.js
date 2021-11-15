214b3f81b8df05022b1ae5bc018a8f9efbc1a50f
elib <elib@itu.dk>
2021-11-15 13:33:04 +0100

    header

--- coadventure/src/App.js
+++ coadventure/src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MenuBar from './components/MenuBar/MenuBar';
 import Footer from './components/Footer/Footer';
 
+
+import Home from './components/MenuBar/Home';
+import Excursions from './components/MenuBar/Excursions';
+import BookSeats from './components/MenuBar/BookSeats';
+import Feedback from './components/MenuBar/Feedback';
+import Profile from './components/MenuBar/Profile';
+import Member from './components/Member/Member';
+import MemberRegistration from './components/Member/SignUp/MemberRegistration';
+import ExcursionInfo from './components/Member/ExcursionInfo';
+import SignUp from './components/SignUp/SignUp';
+
 function App() {
   return (
-    <div>
-      <MenuBar/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <MenuBar/>
+
+        <Switch>
+          <Route path="/excursions" render={Excursions}/>
+          <Route path="/bookSeats" render={BookSeats}/>
+          <Route path="/feedback" render={Feedback}/>
+          <Route path="/profile" render={Profile}/>
+          <Route path="/signUp" render={SignUp}/>
+          
+          <Route path="/member" exact render={Member}/>
+          <Route path="/member/:excursionId" render={ExcursionInfo}/>
+          <Route path="/memberRegistration" render={MemberRegistration}/>
+          <Route path="/" exact render={Home}/>
+        </Switch>  
+
+        <Footer/>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
--- coadventure/src/components/MenuBar/MenuBarItems/Home/Organizer/Organizer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Organizer(){
-    return(
-        <p>Organizer</p>
-    )
-}
-
-export default Organizer;
